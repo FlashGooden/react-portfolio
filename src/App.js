474f7e2dfcdbfb435a2369fc5087a0e77af2ea33
Gamerman2001 <Tro12185@gmail.com>
2019-10-20 19:07:40 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './App.css';
+import indexMain from './pages/IndexMain'
+import Projects from './pages/Projects'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-     hi!
+      <Switch>
+        <Route path='/' exact component={indexMain}/>
+        <Route path='projects' exact component={Projects}/>
+        <Route path='contact' exact component={Contact}/>
+        <Route path='/' exact component={About}/>
+      </Switch>
     </div>
   );
 }
