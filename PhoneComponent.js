d5b072f1fa0975d76dbbd05555d6281fd45ee35b
Troy Gooden <Tro12185@gmail.com>
2019-10-27 22:31:58 -0400

    Update PhoneComponent.js
    Add phone component code

--- PhoneComponent.js
+++ PhoneComponent.js
@@ -1,2 +1,16 @@
 import PhoneStack from "./components/PhoneStack"
 import React, { Fragment } from "react"
+
+function App() {
+  return (
+    <Fragment>
+      {/*This React component is perfect for showcasing your mobile app portfolio!
+      ***All that is required to use this in your portfolio is to replace the images in
+      the folder with ones of your choosing, and import src/components/molecules/PhoneStack.js
+      and PhoneStack.css into your project.*/}
+      <PhoneStack />
+    </Fragment>
+  )
+}
+
+export default App
